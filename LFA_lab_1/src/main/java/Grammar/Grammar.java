52ec148d594f1202550abac75c57c2214f09922f
arturtugui <146761295+arturtugui@users.noreply.github.com>
2025-03-10 02:13:20 +0200

    Finished laboratory 2

--- LFA_lab_1/src/main/java/Grammar/Grammar.java
+++ LFA_lab_1/src/main/java/Grammar/Grammar.java
@@ -1,6 +1,7 @@
 package Grammar;
 
 import FiniteAutomaton.FiniteAutomaton;
+import Grammar.symbols.Epsilon;
 import Grammar.symbols.NonTerminal;
 import Grammar.symbols.Symbol;
 import Grammar.symbols.Terminal;
@@ -86,9 +87,13 @@ public class Grammar {
         return sb;
     }
 
+    // some type 3 grammar can still have multiple nonterminals, my current implementation just finds the 1st
     public FiniteAutomaton toFiniteAutomaton() {
-        //only type 3 grammar can be fa
-        if(g)
+        // only type 3 grammar can be fa
+        if(Grammar.classifyGrammar(this) != 3){
+            System.out.println("Only type 3 grammar can be converted to Finite Automaton");
+            return null;
+        }
 
 
         NonTerminal finalState = new NonTerminal("final");
@@ -105,6 +110,8 @@ public class Grammar {
 
         for (Production p : productions) {
 
+            Symbol fromState = p.left.getFirst();
+
             Symbol terminalSymbol = p.right.stream()
                     .filter(symbol -> symbol instanceof Terminal)
                     .findFirst()
@@ -115,7 +122,7 @@ public class Grammar {
                     .findFirst()
                     .orElse(finalState);
 
-            transitions.add(new Transition(p.left, terminalSymbol,  nonTerminalSymbol));
+            transitions.add(new Transition(fromState, terminalSymbol,  nonTerminalSymbol));
         }
 
         return transitions;
@@ -123,4 +130,152 @@ public class Grammar {
 
 
 
+    /// for lab-2
+    public static int classifyGrammar(Grammar grammar) {
+        boolean isType0 = true;
+        boolean isType1 = true;
+        boolean isType2 = true;
+        boolean isType3 = true;
+
+        boolean hasEpsilonRHS = false;
+        boolean hasMultipleLHS = false;
+        int side = 0;
+
+        for(Production prod : grammar.productions){
+            //System.out.println(prod.toString() + "   " + prod.left.size());
+
+            if(prod.right.getFirst() instanceof Epsilon) {
+                hasEpsilonRHS = true;
+
+                isType1 = false;
+
+                if(hasMultipleLHS) {
+                    //System.out.println("type 0");
+                    break; // is type 0, type 1 cannot have RHS empty
+                }
+            }
+
+            if (prod.left.getFirst() instanceof Epsilon) {
+                //System.out.println("not valid");
+                isType0 = false;
+                isType1 = false;
+                isType2 = false;
+                isType3 = false;
+                break; // not a valid grammar, LHS must be non-empty
+            }
+
+            else if(prod.left.size() > 1) {
+                // can be type 0 or type 1
+                hasMultipleLHS = true;
+                //System.out.println("type 0 or 1");
+                isType2 = false;
+                isType3 = false;
+
+                if(hasEpsilonRHS) {
+                    //System.out.println("type 0");
+                    isType1 = false;
+                    break; // is type 0, type 1 cannot have RHS empty
+                }
+            }
+
+            else {
+                // can be type 2 or type 3
+                //System.out.println("type 2 or 3");
+
+                // count how many non-terminals in RHS
+                long nonTerminalCount = prod.right.stream()
+                        .filter(symbol -> symbol instanceof NonTerminal)
+                        .count();
+
+                // a. if more than one (not type 3)
+                if (nonTerminalCount > 1) {
+                    //System.out.println("type 2 c1");
+                    isType3 = false; // is type 2
+                }
+
+                else if (nonTerminalCount == 0) {
+                    //either empty RHS or no nonTerminals nothing can be said
+                    continue;
+                }
+
+                // find on which side is it
+                else {
+                    // on the left
+                    if(prod.right.get(0) instanceof NonTerminal) {
+                        // c. if side changed
+                        if(side == 1){
+                            //System.out.println("type 2 c2");
+                            isType3 = false; // is type 2
+                        }
+
+                        side = -1;
+                    }
+
+                    // on the right
+                    else if(prod.right.get(prod.right.size() - 1) instanceof NonTerminal) {
+                        // c. if side changed
+                        if(side == -1){
+                            //System.out.println("type 2 c3");
+                            isType3 = false; // is type 2
+                        }
+
+                        side = 1;
+                    }
+
+                    // b. if in the middle
+                    else {
+                        //System.out.println("type 2 c4");
+                        isType3 = false; // is type 2
+                    }
+                }
+            }
+        }
+
+        //System.out.println(hasEpsilonRHS + " " + hasMultipleLHS);
+
+        if(isType3){
+            System.out.println("The grammar is Type 3");
+            return 3;
+        }
+        else if(isType2){
+            System.out.println("The grammar is Type 2");
+            return 2;
+        }
+        else if(isType1){
+            System.out.println("The grammar is Type 1");
+            return 1;
+        }
+        else if(isType0){
+            System.out.println("The grammar is Type 0");
+            return 0;
+        }
+        else{
+            System.out.println("This is not a valid grammar (production with empty string - LHS not allowed)");
+            return -1;
+        }
+    }
+
+    public void displayGrammar() {
+        System.out.println("\tGrammar:");
+        System.out.println("VN (Non-Terminals): " + nonTerminals);
+        System.out.println("VT (Terminals): " + terminals);
+        System.out.println("Start Symbol: " + startSymbol);
+        System.out.println("Productions:");
+
+        for (Production production : productions) {
+            String lhs = production.left.stream()
+                    .map(Symbol::toString)
+                    .reduce((a, b) -> a + b)
+                    .orElse("");
+
+            String rhs = production.right.stream()
+                    .map(Symbol::toString)
+                    .reduce((a, b) -> a + b)
+                    .orElse("ε");  // Handle epsilon productions
+
+            System.out.println(lhs + " → " + rhs);
+        }
+
+        System.out.println("\n");
+    }
 }
---
+++ LFA_lab_1/src/main/java/Grammar/symbols/Epsilon.java
@@ -0,0 +1,8 @@
+package Grammar.symbols;
+
+public class Epsilon extends Symbol {
+    public Epsilon() {
+        super("ε"); // epsilon symbol
+    }
+}
+
